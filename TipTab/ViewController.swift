4e5e5c0cb90816ad1b059d673eedc4fa6c032506
David Bocardo <davsys01@hotmail.com>
2016-09-25 16:22:52 -0600

    Added app icon/background color
    UI design improvements, adding app icon and background color changes. Also now the textfield will get selected upon app launch automatically

--- TipTab/ViewController.swift
+++ TipTab/ViewController.swift
@@ -46,6 +46,8 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        billText.becomeFirstResponder()
     }
     
     override func viewWillAppear(_ animated: Bool) {
